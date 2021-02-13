736e539df5d351a1a72230a2353a66e8ca0b0ca4
Frank Pagan <frank@cocreate.app>
2021-02-13 20:48:02 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "60283a7771064611a678a1a5",
             key: "html",
             data:{
                 name: "Google Maps Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "60283a7771064611a678a1a4",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "60283a7771064611a678a1a5",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/google-maps",
