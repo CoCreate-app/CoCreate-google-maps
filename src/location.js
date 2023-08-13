a51b42d2c026f970cef4afcdd2c6c9f3e5dfef1b
frankpagan <frank@cocreate.app>
2023-08-13 11:04:08 -0600

    feat: Refactor geolocation initialization and saving logic
    - The attribute 'data-geolocation' has been changed to 'geolocation'
    - The variable 'geolocation_html' has been changed to 'element'
    - The method 'showPosition' has been renamed to 'showLocation'
    - The method 'saveData' has been renamed to 'save'
    - The method 'getPositionAsJSon' has been updated to return the position object directly
    - Removed unused code and console logs

--- src/location.js
+++ src/location.js
@@ -4,109 +4,65 @@ import { CoCreateMapAnimate } from './animate.js';
 /*Class Custom*/
 
 var CoCreateMapGetLocation = function () {
-    //init custom
-    /*query mongo
-    > use mydb
-    > db.getCollection("users").find({"_id": ObjectId("5de03b9cc745412976891133")})*/
-    this.attr = 'data-geolocation';
-    this.geolocation_html = null;
+    this.attr = 'geolocation';
+    this.element = null;
 
     this.init = () => {
         if (!this.ccMapAnimate) {
             this.ccMapAnimate = new CoCreateMapAnimate();
         }
-        this.geolocation_html = object.querySelector("[" + this.attr + "]");
-        if (this.geolocation_html && this.geolocation_html.getAttribute(this.attr) == 'true') {
-            console.log("initialite map");
-            this.showPosition();
+        this.element = object.querySelector("[" + this.attr + "]");
+        if (this.element && this.element.getAttribute(this.attr) == 'true') {
+            console.log("initialize map");
+            this.showLocation();
+
+            this.element.setValue = (data) => {
+                this.createAnimator(data.object.currentLocation, data.object, data.object.icon);
+            }
+
+            if (this.element.getValue)
+                this.element.setValue(this.element.getValue())
+
+            this.element.getValue = () => {
+                let coordinates = this.getPositionAsJSon(position)
+                coordinates.icon = ''
+            }
+
+
         }
-        // CoCreateSocket.listen('update.object', (data) =>{
-        //     console.log(data);
-        //     if (data)
-        //         this.createAnimator(data.data.currentLocation, data.object, data.data.icon);
-        // });
     }
 
     this.createAnimator = (position, object, icon) => {
         this.ccMapAnimate.addAnimator(object, { 'lat': position.coords.latitude, 'lng': position.coords.longitude }, icon) // sending data
     }
 
-    this.saveData = (position) => {
-        try {
-            CoCreate.validateKeysJson(this.geolocation_html.dataset, ['array', 'object']);
-            let array = this.geolocation_html.dataset['array'] || '';
-            let object = this.geolocation_html.dataset['object'] || '';
-            // textinput to object
-            let icon = document.querySelector("input[data-animator='icon']").value;
-            console.log(icon);
-            if (icon) {
-                icon = JSON.parse(icon);
-            } else {
-                icon = this.icon;
-            }
-            if (array != '')
-                if (object != '') {
-                    console.log("Saved location in db", object);
-                    let obj = {
-                        method: 'update.object',
-                        array: array,
-                        object: {
-                            _id: object,
-                            currentLocation: this.getPositionAsJSon(position),
-                            icon: icon
-                        }
-                    };
-                    console.log(obj);
-                    CoCreate.crud.send(obj);
-                } else {
-                    object = CoCreate.crud.send({
-                        method: 'create.object',
-                        array: array,
-                        object: {
-                            currentLocation: this.getPositionAsJSon(position),
-                            icon: icon
-                        }
-                    });
-                    console.log(object);
-                    this.geolocation_html.setAttribute("object", object);
-                }
-        }
-        catch (e) {
-            console.error(e);
-        }
+    this.save = () => {
+        this.element.save()
     }
 
     this.getPositionAsJSon = (position) => {
         return {
-            coords: {
-                accuracy: position.coords.accuracy,
-                altitude: position.coords.altitude,
-                altitudeAccuracy: position.coords.altitudeAccuracy,
-                heading: position.coords.heading,
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-                speed: position.coords.speed,
-            },
+            accuracy: position.coords.accuracy,
+            altitude: position.coords.altitude,
+            altitudeAccuracy: position.coords.altitudeAccuracy,
+            heading: position.coords.heading,
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            speed: position.coords.speed,
             timestamp: position.timestamp,
-            geoLocation: {
-                latitude: position.coords.latitude,
-                longitude: position.coords.longitude,
-            }
         };
     }
 
     this.showLocation = (position) => {
-        // console.log(this);
-        // Check position has been changed or not before doing anything
+        // Check if the postion changed
         if (this.prevLat != position.coords.latitude || this.prevLong != position.coords.longitude) {
             // Set previous location
             this.prevLat = position.coords.latitude;
             this.prevLong = position.coords.longitude;
-            // console.log(position);
-            this.saveData(position);
-            var positionInfo = "Your position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
-            // document.getElementById("result").innerHTML = positionInfo;
 
+            this.element.save();
+            let positionInfo = "Your position is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " + position.coords.longitude + ")";
+            console.log(positionInfo);
         }
     }
 
